f1119c022372ecef08af5b9282f073a5fabe93e9
pujara.brijesh1@gmail.com <pujara.brijesh1@gmail.com>
2023-09-28 14:29:31 +0100

    feature: Hero design complete with menu

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "../node_modules/next/link";
 import {
   Cursor,
   useTypewriter,
@@ -16,10 +17,35 @@ function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://media.licdn.com/dms/image/D4E03AQE2_HtbVz7prQ/profile-displayphoto-shrink_400_400/0/1679742728704?e=1701302400&v=beta&t=Hkn9wbMMyRVzODsO6X4Co6-J_bF6g_ZQX6SE6YB4jR0"
+        alt=""
+      />
+      <div className="z-20">
+        <h2 className="uppercase text-sm text-gray-500 pb-2 tracking-[15px]">
+          Software Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
