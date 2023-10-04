ced043da0ae8aa9f2275e853ed5369ec420731fa
pujara.brijesh1@gmail.com <pujara.brijesh1@gmail.com>
2023-10-04 15:18:15 +0100

    feat: Create contact form

---
+++ components/Contact.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { PhoneIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form"
+
+
+type Props = {};
+
+type Inputs = {
+    name: string
+    email: string
+    subject: string
+    message: string
+  }
+
+function Contact({}: Props) {
+  const {
+    register,
+    handleSubmit,
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) =>;
+
+  return (
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <h3 className="absolute top-36 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact{" "}
+      </h3>
+
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
+          Happy to chat, get in touch.{" "}
+          <span className="decoration-[#F7AB0A]/50 underline">
+            {" "}
+            Contact me.
+          </span>
+        </h4>
+
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F8AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">+447538986983</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F8AB0A] h-7 w-7 animate-pulse" />
+            <p className="text-2xl">pujara.brijesh1@gmail.com</p>
+          </div>
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex  space-x-2">
+            <input {...register('name')} placeholder="Name" className="contactInput" type="text" />
+            <input {...register('email')} placeholder="Email" className="contactInput" type="email" />
+          </div>
+
+          <input {...register('subject')} placeholder="Subject" className="contactInput" type="text" />
+
+          <textarea {...register('message')} placeholder="Message" className="contactInput" />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
